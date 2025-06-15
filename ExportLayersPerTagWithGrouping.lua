2d5ee535d4467b98d08d428134da4fd0c8efadf5
NTD <vinhdtq123123123@gmail.com>
2025-06-15 16:33:13 +0700

    Update level 2 folders

--- ExportLayersPerTagWithGrouping.lua
+++ ExportLayersPerTagWithGrouping.lua
@@ -11,41 +11,48 @@
 
     Source and inspiration: https://github.com/PKGaspi/AsepriteScripts
 ]]
--- Import main.
-local err = dofile("main.lua")
+local err = dofile("main.lua") -- Chứa HideLayers, RestoreLayersVisibility, Dirname, ...
 if err ~= 0 then return err end
 
 local n_layers = 0
 
-local function exportLayers(sprite, root_layer, filename, group_sep, data)
+local function exportLayers(sprite, root_layer, pathTemplate, group_sep, data, groupPrefix)
     for _, layer in ipairs(root_layer.layers) do
-        local filename = filename
         if layer.isGroup then
             local previousVisibility = layer.isVisible
             layer.isVisible = true
-            filename = filename:gsub("{layergroups}", layer.name .. group_sep .. "{layergroups}")
-            exportLayers(sprite, layer, filename, group_sep, data)
+            local newGroupPrefix = groupPrefix .. layer.name .. group_sep
+            exportLayers(sprite, layer, pathTemplate, group_sep, data, newGroupPrefix)
             layer.isVisible = previousVisibility
-        else
+        elseif root_layer ~= sprite then
             layer.isVisible = true
-            filename = filename:gsub("{layergroups}", "")
-            filename = filename:gsub("{layername}", layer.name)
-            os.execute("mkdir \"" .. Dirname(filename) .. "\"")
 
+            -- Gán các giá trị format
+            local groupPath = groupPrefix -- Ví dụ: Head/
+            local layername = layer.name -- Ví dụ: Bare
+            local baseFolderPath = pathTemplate
+                :gsub("{layergroups}", groupPath)
+                :gsub("{layername}", layername .. "/")
+
+            -- Tạo thư mục nếu chưa có
+            os.execute('mkdir "' .. Dirname(baseFolderPath) .. '"')
+
+            -- Loại spritesheet
             if data.spritesheet then
                 local sheettype = SpriteSheetType.HORIZONTAL
-                if (data.tagsplit == "To Rows") then
+                if data.tagsplit == "To Rows" then
                     sheettype = SpriteSheetType.ROWS
-                elseif (data.tagsplit == "To Columns") then
+                elseif data.tagsplit == "To Columns" then
                     sheettype = SpriteSheetType.COLUMNS
                 end
 
+                -- Xuất theo tag
                 if #sprite.tags > 0 then
                     for _, tag in ipairs(sprite.tags) do
-                        local tag_filename = filename
-                        tag_filename = tag_filename:gsub("{tagname}", tag.name)
-                        tag_filename = tag_filename:gsub("%." .. data.format .. "$", "")
-                        tag_filename = tag_filename .. "." .. data.format
+                        local tagname = tag.name
+                        local finalPath = baseFolderPath
+                            :gsub("{tagname}", tagname)
+                            :gsub("%." .. data.format .. "$", "") .. "." .. data.format
 
                         app.command.ExportSpriteSheet{
                             ui=false,
@@ -56,7 +63,7 @@ local function exportLayers(sprite, root_layer, filename, group_sep, data)
                             width=0,
                             height=0,
                             bestFit=false,
-                            textureFilename=tag_filename,
+                            textureFilename=finalPath,
                             dataFilename="",
                             dataFormat=SpriteSheetDataFormat.JSON_HASH,
                             borderPadding=0,
@@ -65,17 +72,18 @@ local function exportLayers(sprite, root_layer, filename, group_sep, data)
                             extrude=false,
                             openGenerated=false,
                             layer="",
-                            tag=tag.name,
+                            tag=tagname,
                             splitLayers=false,
                             splitTags=false,
                             listLayers=layer,
                             listTags=false,
                             listSlices=true,
                         }
-
                         n_layers = n_layers + 1
                     end
                 else
+                    -- Không có tag, xuất 1 file
+                    local finalPath = baseFolderPath:gsub("{tagname}", "")
                     app.command.ExportSpriteSheet{
                         ui=false,
                         askOverwrite=false,
@@ -85,7 +93,7 @@ local function exportLayers(sprite, root_layer, filename, group_sep, data)
                         width=0,
                         height=0,
                         bestFit=false,
-                        textureFilename=filename,
+                        textureFilename=finalPath,
                         dataFilename="",
                         dataFormat=SpriteSheetDataFormat.JSON_HASH,
                         borderPadding=0,
@@ -101,12 +109,12 @@ local function exportLayers(sprite, root_layer, filename, group_sep, data)
                         listTags=false,
                         listSlices=true,
                     }
-
                     n_layers = n_layers + 1
                 end
-
             else
-                sprite:saveCopyAs(filename)
+                -- Xuất ảnh đơn
+                local finalPath = baseFolderPath:gsub("{tagname}", "")
+                sprite:saveCopyAs(finalPath)
                 n_layers = n_layers + 1
             end
 
@@ -115,6 +123,7 @@ local function exportLayers(sprite, root_layer, filename, group_sep, data)
     end
 end
 
+-- UI
 local dlg = Dialog("Export layers")
 dlg:file{
     id = "directory",
@@ -125,7 +134,7 @@ dlg:file{
 dlg:entry{
     id = "filename",
     label = "File name format:",
-    text = "{layergroups}{tagname}{layername}"
+    text = "{layergroups}{layername}{tagname}"
 }
 dlg:combobox{
     id = 'format',
@@ -137,14 +146,13 @@ dlg:combobox{
     id = 'group_sep',
     label = 'Group separator:',
     option = '/',
-    options = {'-','/', '_'}
+    options = {'/'}
 }
 dlg:slider{id = 'scale', label = 'Export Scale:', min = 1, max = 10, value = 1}
 dlg:check{
     id = "spritesheet",
     label = "Export as spritesheet:",
-    selected = true,
-	visible = false
+    selected = true
 }
 dlg:combobox{
     id = "tagsplit",
@@ -170,18 +178,20 @@ if output_path == nil then
 end
 
 local group_sep = dlg.data.group_sep
-filename = filename:gsub("{spritename}", RemoveExtension(Basename(Sprite.filename)))
-filename = filename:gsub("{groupseparator}", group_sep)
+filename = filename
+    :gsub("{spritename}", RemoveExtension(Basename(Sprite.filename)))
+    :gsub("{groupseparator}", group_sep)
 
 Sprite:resize(Sprite.width * dlg.data.scale, Sprite.height * dlg.data.scale)
 local layers_visibility_data = HideLayers(Sprite)
-exportLayers(Sprite, Sprite, output_path .. filename, group_sep, dlg.data)
+exportLayers(Sprite, Sprite, output_path .. "/" .. filename, group_sep, dlg.data, "")
 RestoreLayersVisibility(Sprite, layers_visibility_data)
 Sprite:resize(Sprite.width / dlg.data.scale, Sprite.height / dlg.data.scale)
 
-if dlg.data.save then Sprite:saveAs(dlg.data.directory) end
+if dlg.data.save then
+    Sprite:saveAs(dlg.data.directory)
+end
 
 local dlg = MsgDialog("Success!", "Exported " .. n_layers .. " layers.")
 dlg:show()
-
 return 0
